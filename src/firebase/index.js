fa7d405e43d381551cb8cebe0aaf3f84caffcc44
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-09-20 20:28:27 +0530

    Setup firebase for file storage

---
+++ src/firebase/index.js
@@ -0,0 +1,23 @@
+import { getStorage } from 'firebase/storage'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCU3OsvUHdsuJo0vxGzcZk0G7PzYrm4a1g',
+  authDomain: 'taskbox-57a52.firebaseapp.com',
+  projectId: 'taskbox-57a52',
+  storageBucket: 'taskbox-57a52.appspot.com',
+  messagingSenderId: '1066766858686',
+  appId: '1:1066766858686:web:aa8e04d623e701b15921ef',
+  measurementId: 'G-JQ5M0DMDHQ',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app)
+
+const storage = getStorage(app)
+
+export { storage, app as default }

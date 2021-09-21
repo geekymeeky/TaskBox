05be720f568ebefafe68f81652dac6d2e944f75e
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-09-21 22:19:45 +0530

    Update form.js

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -75,7 +75,7 @@ const Form = () => {
       fileUrl,
     }
 
-    await fetch(`https://${window.location.host}:5000/add/`, {
+    await fetch(`https://${window.location.host}/add/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
